c065d54b479be3f74e3a8e6c5911bff1b51b5fd9
amanpatwal123 <amanpatwal102@gmail.com>
2026-09-07 22:03:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+Number_1 = int(input("Enter Your Number:- "))
+
+Number_2 = int(input("Enter Your Number:- "))
+
+print("""Enter What You Wants to Do:
+              Mltipication = 1
+              Division = 2
+              Substraction = 3
+              Addition = 4""")
+ 
+A = int(input("Enter here:- "))
+
+if(A==1):
+	       print("I don't Know 999")
+	       print("You are not a 6 year old boy do it yourself")
+if(A==2):
+	       print("You are duffer")	
+	       print("Abe khud bhi krr le kuch")
+if(A==3):
+	       print("Kathu ko bulau")
+	       print("Ya maths wale ko")
+if(A==4):
+	print("Abe Add krrna bhi nhi ara kya")
+else:
+	print("Abe ulta paida hua tha kya")	
+print("""Error 404
+	    Calculator Is busy""")
